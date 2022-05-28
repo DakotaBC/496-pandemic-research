5eb0e0fa0aa28c3ead8cee057df8159f78d20860
slaterd2 <doulags@gmail.com>
2022-05-27 17:43:31 -0700

    May27

--- DouglasWork/main.py
+++ DouglasWork/main.py
@@ -14,25 +14,11 @@ file = open("/home/douglas/Desktop/CSCI496/github/496-pandemic-research/DouglasW
 #CovidData = pandas.read_csv(file)
 
 data = numpy.genfromtxt(file, delimiter=',',usecols=numpy.arange(start=1, stop=208))
-#data = numpy.delete(data, 1)
-#print(data)
-#data = numpy.delete(data, 0)
 
-#for a in data:
-    #print(a)
-
-#inp = data[:-20]
-#print(inp)
-#targets = data[20:]
-#print()
-#print(targets)
-
-#dataset = keras.preprocessing.timeseries.timeseries_dataset_from_array(inp, targets, 21)
-
-#print(dataset)
+print(data[1])
 
 model = tensorflow.keras.Sequential([tensorflow.keras.layers.Dense(208,activation='relu'),
-                          tensorflow.keras.layers.Dense(208,activation='relu'),
+                          #tensorflow.keras.layers.Dense(208,activation='relu'),
                           #keras.layers.Dense(208,activation='relu')])
                           tensorflow.keras.layers.Dense(1)])
 
@@ -46,29 +32,9 @@ model.compile(loss=tensorflow.keras.losses.BinaryCrossentropy(from_logits=True),
 train = numpy.asarray(data[1:10])
 test = numpy.asarray(data[10:20])
 
-history = model.fit(numpy.all(train), epochs=10,
-                    validation_data=numpy.all(test),
+history = model.fit(train, epochs=10,
+                    validation_data=test,
                     validation_steps=10)
 
-
-#print(features)
-
-#print(CovidData.iloc[:,0:8].values)
-#print(CovidData.iloc[:,8].values)
-
-#scaler_X = preprocessing.StandardScaler()
-#scaler_Y = preprocessing.StandardScaler()
-
-#Inputs = scaler_X.fit_transform(CovidData.iloc[:,8:207].values)
-#Predictor = scaler_Y.fit_transform((CovidData.iloc[:,2].values).reshape(-1,1))
-#print("test")
-#alg = svm.SVR(kernel = 'rbf')
-#alg.fit(Inputs, Predictor.ravel())
-
-#predicted = alg.predict((CovidData.iloc[:,8:207]))
-#predicted = scaler_Y.inverse_transform(predicted.reshape(1,-1))
-#print(predicted)
-
-print("hi!")
 
 
